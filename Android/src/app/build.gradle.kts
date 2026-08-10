3562a8c38292354e4a4044939de53462a6d5ea35
realizer-1966 <realizer-1966@users.noreply.github.com>
2026-08-10 11:06:50 +0000

    fix: set HuggingFace OAuth redirect scheme

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     // Needed for HuggingFace auth workflows.
     // Use the scheme of the "Redirect URLs" in HuggingFace app.
     manifestPlaceholders["appAuthRedirectScheme"] =
-        "REPLACE_WITH_YOUR_REDIRECT_SCHEME_IN_HUGGINGFACE_APP"
+        "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"
     manifestPlaceholders["applicationName"] = "com.google.ai.edge.gallery.GalleryApplication"
     manifestPlaceholders["appIcon"] = "@mipmap/ic_launcher"
 
